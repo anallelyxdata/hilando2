0d48235a909f32d56aef68d69db19ca45db2437d
anallelyxdata <anallely@x-data.mx>
2024-03-20 03:23:00 -0600

    correcciones de diseño sin animaciones

--- js/diseno.js
+++ js/diseno.js
@@ -6,9 +6,10 @@ gsap.to(sections, {
   xPercent: -100 * (sections.length - 1),
   ease: "none",
   scrollTrigger: {
-    trigger: ".container",
+    trigger: ".section_diseno",
     pin: true,
     scrub: 2,
+    markers:true,
     // snap: 1 / (sections.length - 1),
     end: () => "+=" + document.querySelector(".container").offsetWidth
   }
@@ -30,52 +31,51 @@ document.querySelector('.dis2_button').addEventListener('mouseout', function() {
     document.querySelector('.dis2_button').style.animationName='pulse-animation'
 });
 
-document.querySelector('.dis3_button').addEventListener('mouseover', function() {
-    const revealElements = document.querySelectorAll('.dis3_revealp');
-    revealElements.forEach(function(element) {
-        element.style.opacity = '1';
-    });
-    document.querySelector('.dis3_button').style.animationName='none'
+// 3
+document.querySelector('.dis3_button1').addEventListener('mouseover', function() {
+    document.querySelector('#dis3_reveal1').style.opacity='1'
+    document.querySelector('.dis3_button1').style.animationName='none'
 });
 
-document.querySelector('.dis3_button').addEventListener('mouseout', function() {
-    const revealElements = document.querySelectorAll('.dis3_revealp');
-    revealElements.forEach(function(element) {
-        element.style.opacity = '0';
-    });
-    document.querySelector('.dis3_button').style.animationName='pulse-animation'
+document.querySelector('.dis3_button1').addEventListener('mouseout', function() {
+    document.querySelector('#dis3_reveal1').style.opacity='0'
+    document.querySelector('.dis3_button1').style.animationName='pulse-animation'
 });
 
 
 document.querySelector('.dis3_button2').addEventListener('mouseover', function() {
-    document.querySelector('.materiales').style.opacity='1'
+    document.querySelector('#dis3_reveal2').style.opacity='1';
+    document.querySelector('.dis3_button2').style.animationName='none'
 });
 
 document.querySelector('.dis3_button2').addEventListener('mouseout', function() {
-    document.querySelector('.materiales').style.opacity='0'
+    document.querySelector('#dis3_reveal2').style.opacity='0';
+    document.querySelector('.dis3_button2').style.animationName='pulse-animation'
 });
 
+// 4
 document.querySelector('.dis4_button1').addEventListener('mouseover', function() {
-    document.querySelector('.dis4_col1_reveal').style.opacity='1'
+    document.querySelector('.dis4_reveal1').style.opacity='1'
 });
 
 document.querySelector('.dis4_button1').addEventListener('mouseout', function() {
-    document.querySelector('.dis4_col1_reveal').style.opacity='0'
+    document.querySelector('.dis4_reveal1').style.opacity='0'
 });
 
 document.querySelector('.dis4_button2').addEventListener('mouseover', function() {
-    document.querySelector('.dis4_col2_reveal').style.opacity='1'
+    document.querySelector('.dis4_reveal2').style.opacity='1'
 });
 
 document.querySelector('.dis4_button2').addEventListener('mouseout', function() {
-    document.querySelector('.dis4_col2_reveal').style.opacity='0'
+    document.querySelector('.dis4_reveal2').style.opacity='0'
 });
 
-document.querySelector('.dis5_button2').addEventListener('mouseover', function() {
-    document.querySelector('.dis5_col1_reveal').style.opacity='1'
+// 5
+document.querySelector('.dis5_button1').addEventListener('mouseover', function() {
+    document.querySelector('.dis5_reveal1').style.opacity='1'
 });
 
-document.querySelector('.dis5_button2').addEventListener('mouseout', function() {
-    document.querySelector('.dis5_col1_reveal').style.opacity='0'
+document.querySelector('.dis5_button1').addEventListener('mouseout', function() {
+    document.querySelector('.dis5_reveal1').style.opacity='0'
 });
 
--- js/gsap-diseno.js
+++ js/gsap-diseno.js
@@ -1,128 +1,143 @@
-// // Register the ScrollTrigger plugin
+// // // Register the ScrollTrigger plugin
 // gsap.registerPlugin(ScrollTrigger);
 
 
-// // Create a GSAP timeline with the animation
-// const pensar = gsap.timeline({
+// const disenoTest = gsap.timeline({
 //   scrollTrigger: {
-//     trigger: '.dis_aux01',  
-//     start: 'top center',  
-//     scrub: true,    
-//      markers: false,
-//   },
-// });
-// pensar.fromTo('#animatedPath1', { strokeDashoffset: 3000}, { strokeDashoffset: 0});
-// pensar.to('.dis1_variedad', {
-//   color:'#fff'
-// });
-
-// // Create a GSAP timeline with the animation
-// const dis1 = gsap.timeline({
-//   scrollTrigger: {
-//     trigger: '.dis_aux1',  
-//     start: 'top bottom',  
-//     scrub: true,    
-//      markers: false,
-//   },
-// });
-// dis1.fromTo('#animatedPath2', { strokeDashoffset: 3000 }, { strokeDashoffset: 0 });
-// dis1.to('.dis2_texto', {
-//   opacity:1
-// });
-
-// // Create a GSAP timeline with the animation
-// const dis2 = gsap.timeline({
-//   scrollTrigger: {
-//     trigger: '.dis_aux2',  
+//     trigger: '.section_diseno',  
 //     start: 'top top',  
-//     scrub: true,    
-//      markers: false,
+//     scrub: 1,    
+//     markers: true, 
+//     pin:'.container-dis',
+//     pinSpacing:false,
 //   },
 // });
-// dis2.fromTo('#animatedPath3', { strokeDashoffset: 3000 }, { strokeDashoffset: 0});
-// dis2.to('.dis3_texto', {
-//   opacity:1
-// });
+// // Produccion 1
+//   disenoTest.to('.produccion1_texto', {
+//     top:'0'
+//   },);
 
-// // Create a GSAP timeline with the animation
-// const dis3 = gsap.timeline({
-//   scrollTrigger: {
-//     trigger: '.dis_aux2',  
-//     start: 'top bottom',  
-//     scrub: true,    
-//      markers: false,
-//   },
-// });
-// dis3.fromTo('#animatedPath3', { strokeDashoffset: 3000 }, { strokeDashoffset: 0, duration: 2 });
+// // // Create a GSAP timeline with the animation
+// // const pensar = gsap.timeline({
+// //   scrollTrigger: {
+// //     trigger: '.dis_aux01',  
+// //     start: 'top center',  
+// //     scrub: true,    
+// //      markers: false,
+// //   },
+// // });
+// // pensar.fromTo('#animatedPath1', { strokeDashoffset: 3000}, { strokeDashoffset: 0});
+// // pensar.to('.dis1_variedad', {
+// //   color:'#fff'
+// // });
+
+// // // Create a GSAP timeline with the animation
+// // const dis1 = gsap.timeline({
+// //   scrollTrigger: {
+// //     trigger: '.dis_aux1',  
+// //     start: 'top bottom',  
+// //     scrub: true,    
+// //      markers: false,
+// //   },
+// // });
+// // dis1.fromTo('#animatedPath2', { strokeDashoffset: 3000 }, { strokeDashoffset: 0 });
+// // dis1.to('.dis2_texto', {
+// //   opacity:1
+// // });
+
+// // // Create a GSAP timeline with the animation
+// // const dis2 = gsap.timeline({
+// //   scrollTrigger: {
+// //     trigger: '.dis_aux2',  
+// //     start: 'top top',  
+// //     scrub: true,    
+// //      markers: false,
+// //   },
+// // });
+// // dis2.fromTo('#animatedPath3', { strokeDashoffset: 3000 }, { strokeDashoffset: 0});
+// // dis2.to('.dis3_texto', {
+// //   opacity:1
+// // });
+
+// // // Create a GSAP timeline with the animation
+// // const dis3 = gsap.timeline({
+// //   scrollTrigger: {
+// //     trigger: '.dis_aux2',  
+// //     start: 'top bottom',  
+// //     scrub: true,    
+// //      markers: false,
+// //   },
+// // });
+// // dis3.fromTo('#animatedPath3', { strokeDashoffset: 3000 }, { strokeDashoffset: 0, duration: 2 });
 
 
 
-// // Create a GSAP timeline with the animation
-// const dis4 = gsap.timeline({
-//   scrollTrigger: {
-//     trigger: '.dis_aux3',  
-//     start: 'top center',  
-//     scrub: true,    
-//      markers: false,
-//   },
-// });
-// dis4.fromTo('#animatedPath4', { strokeDashoffset: 3000 }, { strokeDashoffset: 0, duration: 2 });
-// dis4.to('.dis4_texto', {
-//   opacity:1
-// });
+// // // Create a GSAP timeline with the animation
+// // const dis4 = gsap.timeline({
+// //   scrollTrigger: {
+// //     trigger: '.dis_aux3',  
+// //     start: 'top center',  
+// //     scrub: true,    
+// //      markers: false,
+// //   },
+// // });
+// // dis4.fromTo('#animatedPath4', { strokeDashoffset: 3000 }, { strokeDashoffset: 0, duration: 2 });
+// // dis4.to('.dis4_texto', {
+// //   opacity:1
+// // });
 
-// // Create a GSAP timeline with the animation
-// const dis5 = gsap.timeline({
-//   scrollTrigger: {
-//     trigger: '.dis_aux4',  
-//     start: 'top center',  
-//     scrub: true,    
-//      markers: false,
-//   },
-// });
-// dis5.fromTo('#animatedPath5', { strokeDashoffset: 3000 }, { strokeDashoffset: 0, duration: 2 });
-// dis5.to('.dis5_texto', {
-//   opacity:1
-// });
+// // // Create a GSAP timeline with the animation
+// // const dis5 = gsap.timeline({
+// //   scrollTrigger: {
+// //     trigger: '.dis_aux4',  
+// //     start: 'top center',  
+// //     scrub: true,    
+// //      markers: false,
+// //   },
+// // });
+// // dis5.fromTo('#animatedPath5', { strokeDashoffset: 3000 }, { strokeDashoffset: 0, duration: 2 });
+// // dis5.to('.dis5_texto', {
+// //   opacity:1
+// // });
 
-// // Create a GSAP timeline with the animation
-// const dis6 = gsap.timeline({
-//   scrollTrigger: {
-//     trigger: '.dis_aux5',  
-//     start: 'top center',  
-//     scrub: true,    
-//      markers: false,
-//   },
-// });
-// dis6.fromTo('#animatedPath6', { strokeDashoffset: 3000 }, { strokeDashoffset: 0, duration: 2 });
-// dis6.to('.dis6_texto', {
-//   opacity:1
-// });
+// // // Create a GSAP timeline with the animation
+// // const dis6 = gsap.timeline({
+// //   scrollTrigger: {
+// //     trigger: '.dis_aux5',  
+// //     start: 'top center',  
+// //     scrub: true,    
+// //      markers: false,
+// //   },
+// // });
+// // dis6.fromTo('#animatedPath6', { strokeDashoffset: 3000 }, { strokeDashoffset: 0, duration: 2 });
+// // dis6.to('.dis6_texto', {
+// //   opacity:1
+// // });
 
-// // Create a GSAP timeline with the animation
-// const dis7 = gsap.timeline({
-//   scrollTrigger: {
-//     trigger: '.dis_aux6',  
-//     start: 'top center',  
-//     scrub: true,    
-//      markers: false,
-//   },
-// });
-// dis7.fromTo('#animatedPath7', { strokeDashoffset: -3000 }, { strokeDashoffset: 0 },0);
-// dis7.to('.dis7_texto', {
-//   opacity:1
-// },1);
+// // // Create a GSAP timeline with the animation
+// // const dis7 = gsap.timeline({
+// //   scrollTrigger: {
+// //     trigger: '.dis_aux6',  
+// //     start: 'top center',  
+// //     scrub: true,    
+// //      markers: false,
+// //   },
+// // });
+// // dis7.fromTo('#animatedPath7', { strokeDashoffset: -3000 }, { strokeDashoffset: 0 },0);
+// // dis7.to('.dis7_texto', {
+// //   opacity:1
+// // },1);
 
-// // Create a GSAP timeline with the animation
-// const dis8 = gsap.timeline({
-//   scrollTrigger: {
-//     trigger: '.dis_aux7',  
-//     start: 'top top',  
-//     scrub: true,    
-//      markers: false,
-//   },
-// });
-// dis8.fromTo('#animatedPath8', { strokeDashoffset: 3000 }, { strokeDashoffset: 0 });
-// dis8.to('.dis8_texto', {
-//   opacity:1
-// });
+// // // Create a GSAP timeline with the animation
+// // const dis8 = gsap.timeline({
+// //   scrollTrigger: {
+// //     trigger: '.dis_aux7',  
+// //     start: 'top top',  
+// //     scrub: true,    
+// //      markers: false,
+// //   },
+// // });
+// // dis8.fromTo('#animatedPath8', { strokeDashoffset: 3000 }, { strokeDashoffset: 0 });
+// // dis8.to('.dis8_texto', {
+// //   opacity:1
+// // });
